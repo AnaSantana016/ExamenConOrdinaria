65df43f825c47329dac9230e87ba40a6958fd215
orrub <orrub@LAPTOP-KUHTV423>
2021-02-02 13:00:35 +0100

    Vesion 5, Purchase.

--- src/purchase/model/Catalogue.java
+++ src/purchase/model/Catalogue.java
@@ -1,7 +1,25 @@
 package purchase.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Catalogue {
     
     private final String name;
+    
+    private final List<Product> products;
+
+    public Catalogue(String name) {
+        products = new ArrayList<>();
+        this.name = name;
+    }
+    
+    public void takeOff(int pos){
+        products.remove(pos);
+    }
+    
+    public void add(Product product){
+        products.add(product);
+    }
     
 }
